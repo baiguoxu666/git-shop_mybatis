a6cc524fb8509e54c50867fbed3ae31e0b7770c2
baiguoxu666 <928310018@qq.com>
2020-12-24 14:02:51 +0800

    master

--- src/main/java/com/bgx/controller/CategoryController.java
+++ src/main/java/com/bgx/controller/CategoryController.java
@@ -28,6 +28,7 @@ public class CategoryController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("这是master分支");
         return null;
     }
 }
