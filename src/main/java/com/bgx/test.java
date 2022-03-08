be97d07ec9fef87fabb295eeae4029b74917c87c
baiguoxu666 <928310018@qq.com>
2022-03-08 17:07:15 +0800

    克隆提交

---
+++ src/main/java/com/bgx/test.java
@@ -0,0 +1,12 @@
+package com.bgx;
+
+/**
+ * @author: 大神
+ * @date: 2022/3/8 17:03
+ * @description: 加油呀冲出去
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("克隆下来的项目添加");
+    }
+}
